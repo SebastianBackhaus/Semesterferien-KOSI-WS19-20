dfa60103cc53e6d29fced660e54f397fbb86c662
SebastianBackhaus <62331649+SebastianBackhaus@users.noreply.github.com>
2020-03-19 10:45:35 +0100

    FizzBuzz Junit5

---
+++ Junit5FizzBuzz/src/test/java/TestFizzBuzz.java
@@ -0,0 +1,17 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Test.*;
+
+public class TestFizzBuzz {
+
+    @Test
+
+    void test() {
+        System.out.println("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,10,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz");
+
+
+
+    }
+
+}
