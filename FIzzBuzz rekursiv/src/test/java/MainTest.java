1496cdef7437a98479ed3ed75d19e59086eb570b
SebastianBackhaus <62331649+SebastianBackhaus@users.noreply.github.com>
2020-03-19 10:56:55 +0100

    FizzBuzz rekursiv

---
+++ FIzzBuzz rekursiv/src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void fizzBuzz() {
+        System.out.println(
+        "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19"
+        );
+    }
+
+    @Test
+    void main() {
+        
+    }
+}
