c895f3362733dc68c6bd01f1788be3dae518381a
SebastianBackhaus <62331649+SebastianBackhaus@users.noreply.github.com>
2020-03-23 07:06:21 +0100

    Fizz Buzz

--- Fizz Buzz/src/main/java/de/sebastian/FizzBuzz/Main.java
+++ Fizz Buzz/src/main/java/de/sebastian/FizzBuzz/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void  main(String[] args) {
 
-        System.out.println(FizzBuzznormal(1,20));
+        System.out.println (FizzBuzznormal(1,20));
 
-        System.out.println("Rekursiv: ");
+        System.out.println ("Rekursiv: ");
 
         LinkedList<String>Listerekursiv= new LinkedList();
 
@@ -23,18 +23,17 @@ public class Main {
 
         for (int i = min; i < max; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                Liste1.add("Fizz-Buzz");
+                Liste1.add ("Fizz-Buzz");
             } else if (i % 3 == 0) {
-                Liste1.add("Fizz");
+                Liste1.add ("Fizz");
             } else if (i % 5 == 0) {
-                Liste1.add("Buzz");
+                Liste1.add ("Buzz");
             } else {
-                Liste1.add(""+i);
+                Liste1.add (""+i);
             }
         }
 
         return Liste1;
-
     }
 
 
@@ -43,19 +42,17 @@ public class Main {
 
     public static void FizzBuzzRekursiv(int x, LinkedList<String> Liste2,int max) {
 
-
-
         int index = x;
 
         if (index < max) {
             if (index % 3 == 0 && index % 5 == 0) {
-                Liste2.add("Fizz-Buzz");
+                Liste2.add ("Fizz-Buzz");
             } else if (index % 3 == 0) {
-                Liste2.add("Fizz");
+                Liste2.add ("Fizz");
             } else if (index % 5 == 0) {
-               Liste2.add("Buzz");
+               Liste2.add ("Buzz");
             } else {
-                Liste2.add(""+index);
+                Liste2.add (""+index);
             }
             index ++;
             FizzBuzzRekursiv (index,Liste2,max);
